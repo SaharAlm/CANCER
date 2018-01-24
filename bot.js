f8a8439d2f85f08dfc473878a50ba1d68171a004
Twins1 <33881658+Twins1@users.noreply.github.com>
2018-01-24 19:14:52 +0200

    Initial commit

---
+++ bot.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const token = settings.token;
+const prefix = settings.prefix;
+const setting = require("./setting.json")
+
+var bot = new Discord.Client();
+
+bot.on("guildMemberAdd", member => {
+  if (member.guild.id === "405769057065762826") {
+    member.addRole('405754175113265152')
+  }
+  member.guild.channels.get('405769057065762826').send(`**${member.user.username}** Another Nibba join`)
+});
+
+bot.on("guildMemberRemove", (member) => {
+  member.guild.channels.get('405769057065762826').send(`**${member.user.username}** Another Nibba Left `)
+});
