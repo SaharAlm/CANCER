d17598baee9b9b7a1dd87e0e0bf0b1568227a030
Twins1 <33881658+Twins1@users.noreply.github.com>
2018-01-25 13:43:49 +0200

    dickshtain]

--- bot.js
+++ bot.js
@@ -3,8 +3,14 @@ const settings = require("./settings.json")
 const token = settings.token;
 const prefix = settings.prefix;
 
-
+// --var--
 var bot = new Discord.Client();
+
+// need
+
+bot.on('ready', (msg) {
+  msg.channel.send("bitch im here")
+});
 
 bot.on("guildMemberAdd", member => {
   var role = member.guild.roles.find('name', 'user')
